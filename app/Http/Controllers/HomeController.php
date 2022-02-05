7eddbd48a27fba37382b7056ac27f06d90708aca
nanda_yana <nandayana0202@gmail.com>
2022-02-05 23:52:02 +0700

    Integration landing page, checkout, dashboard

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Checkout;
+use Auth;
+
+class HomeController extends Controller
+{
+    public function dashboard()
+    {
+        $checkouts = Checkout::with('Camp')->where('user_id', Auth::id())->get();
+        return view('user.dashboard', ['checkouts' => $checkouts]);
+    }
+}
